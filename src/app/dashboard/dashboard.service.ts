7d00b432ff577f1dd76519e9c4aa241252b90661
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-05-30 00:19:18 -0300

    deploy

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://santander-dio-myszko.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://santander-dio-myszko.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
